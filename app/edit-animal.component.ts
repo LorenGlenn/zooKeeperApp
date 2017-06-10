28a85c6f97de7df22b334a196657472ad109f5ea
peter <pkarella27@yahoo.com>
2017-06-09 20:55:28 -0700

    both pipes working

--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -9,6 +9,14 @@ import { Animal } from './animal.model';
   <h3>Edit Animal</h3>
     <label>Enter info:</label>
     <input [(ngModel)]="childSelectedAnimal.species">
+    <input [(ngModel)]="childSelectedAnimal.name">
+    <input [(ngModel)]="childSelectedAnimal.age">
+    <input [(ngModel)]="childSelectedAnimal.diet">
+    <input [(ngModel)]="childSelectedAnimal.location">
+    <input [(ngModel)]="childSelectedAnimal.caretakers">
+    <input [(ngModel)]="childSelectedAnimal.sex">
+    <input [(ngModel)]="childSelectedAnimal.likes">
+    <input [(ngModel)]="childSelectedAnimal.dislikes">
     <button (click)="doneButtonClicked()">Done</button>
   </div>
 
